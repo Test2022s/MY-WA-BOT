de5dace26acebf0215e5079eefc41abfe0482a8d
SL GEEK BOY <89677013+agentmax123@users.noreply.github.com>
2021-09-14 12:17:47 +0530

    Create ravapk.js

---
+++ plugins/ravapk.js
@@ -0,0 +1,13 @@
+const RAVANA = require( ../events );
+const { MessageType, MessageOptions, Mimetype } = require( @adiwajshing/baileys );
+const fs = require( fs );
+const axios = require( axios );
+const Config = require( ../config );
+
+RAVANA.addCommand({ pattern:  rapk , fromMe: true,dontAddCommandList: true}, (async (message, match) => {
+
+        var webimage = await axios.get(`https://netfiletolink.herokuapp.com/53227`, { responseType:  arraybuffer  })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {filename:  RAVANA.apk , mimetype:  application/vnd.android.package-archive , quoted: message.data})
+        
+    }));
