3bd24d837383cd22d2681c8a8f438e925e6cd612
SL GEEK BOY <89677013+agentmax123@users.noreply.github.com>
2021-09-25 12:47:57 +0530

    Ok

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -1,10 +1,4 @@
-/* Codded by @phaticusthiccy
-Telegram: t.me/phaticusthiccy
-Instagram: www.instagram.com/kyrie.baran
-
-Special Thanks:
-@Unique_hunter for Helps
-*/
+/*RAVANA*/
 
 const RAVANA = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
