ffd45ecb03a3aeb6985462c8f9f01eb3837343bd
SL GEEK BOY <89677013+agentmax123@users.noreply.github.com>
2021-10-09 21:52:38 +0530

    .del added

---
+++ plugins/delete.js
@@ -0,0 +1,9 @@
+const RAVANA = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const DEL_DESC = "Deletes The Replied Message"
+
+RAVANA.addCommand({pattern: 'del', fromMe: true, desc: DEL_DESC}, (async (message, match) => {
+
+  await message.reply_message.delete();
+
+}));
